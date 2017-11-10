561a8745171f4df9010fef356512f7abfcafe0d6
LongTianPy <tianlongapp@gmail.com>
2017-11-10 13:37:20 -0500

    debug

--- get_lineage_by_taxid.py
+++ get_lineage_by_taxid.py
@@ -10,7 +10,7 @@ Entrez.email = "aaa@bbb.com"
 
 # FUNCTIONS
 def get_lineage(metadata_file):
-    df = pd.read_table(metadata_file,header=0,sep=",")
+    df = pd.read_csv(metadata_file,header=0)
     lineage_dict = {'superkingdom':[],'phylum':[],'class':[],'order':[],'family':[],'genus':[],'species':[],'subspecies':[]}
     for i in df.index:
         taxid = df.get_value(i,"Tax_ID")
