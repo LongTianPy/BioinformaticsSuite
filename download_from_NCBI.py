f1fc5bdd7d92932863a96d1786b291679dd747f9
LongTianPy <tianlongapp@gmail.com>
2019-02-04 15:29:18 -0500

    new scripts

---
+++ download_from_NCBI.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+"""
+"""
+
+# IMPORT
+from Bio import Entrez
+from Bio import SeqIO
+from os.path import join
+import argparse
+import os
+import urllib.request
+
+
+# VARIABLES
+Entrez.email = 'aaa@bbb.ccc'
+wgs_base_url = 'ftp://ftp.ncbi.nlm.nih.gov/sra/wgs_aux/'
+
+# FUNCTIONS
+def get_parsed_args():
+    parser = argparse.ArgumentParser(
+        description="Download sequence by accession numbers or so, input is always a file with accession numbers, each per line.")
+    parser.add_argument("-i", dest='file', help="File with accession numbers, one per line.")
+    parser.add_argument("-o", dest='out_dir', help="Target file folder.")
+    args = parser.parse_args()
+    return args
+
+# def parse_accession(accession,out_dir):
+#     handle = Entrez.efetch(db='nucleotide', id=accession, rettype='fasta', retmode='text')
+#     record = SeqIO.read(handle, 'fasta')
+#     num_of_contigs = len(record.seq)
+#     suffix = range(1,num_of_contigs+1)
+#     total_digits = len(accession)
+#     scaffolds = []
+#     for each in suffix:
+#         each_str = str(each)
+#         each_digits = len(each_str)
+#         each_accession = accession[:-each_digits] + each_str
+#         print(each_accession)
+#         handle = Entrez.efetch(db='nucleotide', id=each_accession, rettype='fasta', retmode='text')
+#         record = SeqIO.read(handle,'fasta')
+#         scaffolds.append(record)
+#     with open(join(out_dir,accession+".fasta"),"w") as f:
+#         SeqIO.write(scaffolds,f,'fasta')
+
+def download_from_accession_file(accession_file,out_dir):
+    with open(accession_file, "r") as f:
+        accession_numbers = [i.strip() for i in f.readlines()]
+    for accession in accession_numbers:
+        first_two = accession[:2]
+        second_two = accession[2:4]
+        project = first_two+second_two+"01"
+        url = "/".join([wgs_base_url, first_two, second_two, project, project+".1.fsa_nt.gz"])
+        urllib.request.urlretrieve(url, join(out_dir, accession+".fasta.gz"))
+        # print(url)
+        # cmd = "curl -o {0} {1}".format(out_dir, url)
+        # print(cmd)
+        # os.system(cmd)
+
+def main():
+    args = get_parsed_args()
+    accession_file = args.file
+    out_dir = args.out_dir
+    download_from_accession_file(accession_file,out_dir)
+
+# MAIN
+if __name__ == '__main__':
+    main()
---
+++ recreate_signatures.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+"""
+Sourmash is releasing its 2.0 version and the old signatures are no more supported.
+Here we recreate the signatures with sourmash 2.0 to a different folder
+"""
+
+# IMPORT
+from MySQLdb import Connect
+import pandas as pd
+import os
+from os.path import join
+
+# VARIABLES
+sourmash_dir = "/home/linproject/Workspace/Sourmash2.0/all_sketches/"
+rep_bac_dir = "/home/linproject/Workspace/Sourmash2.0/rep_bac/"
+sourmash_tmp = "/home/linproject/Workspace/Sourmash2.0/tmp_2/"
+sourmash_result = "/home/linproject/Workspace/Sourmash2.0/result/"
+
+# FUNCTIONS
+def connect_to_db():
+    conn = Connect("localhost", "LINbase","Latham@537")
+    c = conn.cursor()
+    c.execute("use LINdb_NCBI_RefSeq_test")
+    return conn, c
+
+def fetch_genomes(c):
+    c.execute("SELECT LIN.Genome_ID, LIN.LIN,Genome.FilePath FROM Genome,LIN WHERE Genome.Genome_ID=LIN.Genome_ID and LIN.Scheme_ID=4")
+    tmp = c.fetchall()
+    Genome_ID = [int(i[0]) for i in tmp]
+    LIN = [i[1] for i in tmp]
+    FilePath = [i[2] for i in tmp]
+    df = pd.DataFrame()
+    df["LIN"] = LIN
+    df["FilePath"] = FilePath
+    df.index = Genome_ID
+    return df
+
+def recompute_signatures(df):
+    def get_LINgroup(genome_id):
+        lin = df.loc[genome_id,'LIN']
+        lingroup = ",".join(lin.split(",")[:6])
+        return lingroup
+    def compute_single_signature(genome_id,folder):
+        cmd = "sourmash compute {0} -k 21,31,51 -n 1000 -o {1} > /dev/null 2>&1".format(df.loc[genome_id,"FilePath"], join(folder,str(genome_id)+".sig"))
+        os.system(cmd)
+    for genome_id in df.index:
+        lingroup = get_LINgroup(genome_id)
+        lingroup_folder = join(sourmash_dir, lingroup)
+        if not os.path.isdir(lingroup_folder):
+            os.mkdir(lingroup_folder)
+            compute_single_signature(genome_id,rep_bac_dir)
+        compute_single_signature(genome_id, lingroup_folder)
+
+# MAIN
+if __name__ == '__main__':
+    conn, c = connect_to_db()
+    df = fetch_genomes(c)
+    recompute_signatures(df)
